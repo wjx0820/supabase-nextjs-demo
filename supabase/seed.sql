df265bc409c0f7176f13099d7251b4b9fc4434be
wjx0820 <1989wjx@gmail.com>
2024-07-23 11:32:35 +0800

    add comments

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -2,4 +2,11 @@ insert into posts (title, content, is_published)
   values 
    ('First post', 'This is my first post.', true),
    ('Second post', 'This is my second post.', false),
-   ('Third post', 'This is my third post.', true)
+   ('Third post', 'This is my third post.', true);
+
+insert into comments (content, post_id) 
+  values 
+   ('First comment', ( select id from posts where title = 'First post')),
+   ('Second comment', ( select id from posts where title = 'First post')),
+   ('Third comment', ( select id from posts where title = 'Second post'));
+  
