dadd3ea1e09ed3051f5ae41612b80c1b6b8677fc
wjx0820 <1989wjx@gmail.com>
2024-07-23 11:41:38 +0800

    nextjs dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const supabase = createClient(
